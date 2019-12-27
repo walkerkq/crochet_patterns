1289f470017a0f110e93c8b9fd386101f1350c18
walkerkq <walkerkq2@gmail.com>
2019-12-27 15:39:24 -0600

    formatting

--- pattern_details_ravelry.py
+++ pattern_details_ravelry.py
@@ -2,11 +2,13 @@ import json
 import os
 import pandas as pd
 import requests
-
 from pandas.io.json import json_normalize
 
-def ravelry_get(url, params, json_object, transpose, output_object = 'dataframe') :
-
+def ravelry_get(url, params, json_object, transpose = False) :
+    '''
+    Pass credentials and parameters to the Ravelry API
+    Return a pandas Dataframe
+    '''
     ravelry_user = os.environ['RAVELRY_USER']
     ravelry_pw = os.environ['RAVELRY_PW']
 
@@ -17,21 +19,22 @@ def ravelry_get(url, params, json_object, transpose, output_object = 'dataframe'
       params = params,
       auth=(ravelry_user,ravelry_pw))
 
+    # convert response to json
     response_json = response.json()
 
-    if output_object == 'json' :
+    # convert json into a string, then to pandas df
+    response_df = pd.read_json(json.dumps(response_json[json_object]))
 
-      response_df = response_json[json_object]
-
-    else : # return dataframe
-      # turn json into a string, convert to pandas df, transpose
-      response_df = pd.read_json(json.dumps(response_json[json_object]))
-      if transpose :
-        response_df = response_df.transpose()
+    if transpose :
+      response_df = response_df.transpose()
 
     return response_df
 
 def ravelry_pattern_search(sub_category) :
+    '''
+    Search patterns in the Ravelry API using a sub-category filter
+    Return a pandas DataFrame of pattern ids and basic details
+    '''
 
     search_results = ravelry_get(
       url = '/patterns/search.json',
@@ -39,14 +42,16 @@ def ravelry_pattern_search(sub_category) :
       'pc': sub_category,
       'page_size' : 100
       },
-      json_object = 'patterns',
-      transpose = False)
+      json_object = 'patterns')
 
     search_results['sub_category_permalink'] = sub_category
 
     return search_results
 
 def ravelry_pattern_details(input_id) :
+    '''
+    Return pattern details by Ravelry pattern id
+    '''
 
     return ravelry_get(
       url = '/patterns.json',
@@ -59,8 +64,7 @@ def ravelry_pattern_details(input_id) :
 pattern_categories_meta = ravelry_get(
   url = '/pattern_categories/list.json',
   params = None,
-  json_object = 'pattern_categories',
-  transpose = False)
+  json_object = 'pattern_categories')
 
 # Unnest the sub_categories
 pattern_categories = json_normalize(
@@ -93,38 +97,3 @@ pattern_details = pattern_details.drop(columns = ['craft', 'pattern_author'], ax
 
 # Write the df to csv
 pattern_details.to_csv('pattern_details.csv', index = False)
-
-### appendix
-# cols that may need to be unpacked
-# lists
-#pattern_attributes - id/permalink (tags basically)
-#pattern_needle_sizes
-#yarn_weight
-
-# def expand_json_attributes(column, item_index = None) :
-
-#   if type(pattern_details.loc[0,column]) is list :
-
-#     attributes = [json_normalize(item[item_index]) if item_index else json_normalize(item) for item in pattern_details[column]]
-#     attributes = pd.concat(attributes, sort = False, ignore_index = True).add_prefix(column + '_')
-
-#   else :
-#     attributes = json_normalize(data = pattern_details[column]).add_prefix(column + '_')
-
-#   return attributes
-
-# categories = pd.DataFrame()
-# for index, row in pattern_categories.iterrows() :
-
-#     # isolate one nested dictionary (the category)
-#     outer_children = pd.DataFrame(row.get('children'))
-#     category = outer_children.get('permalink')
-
-#     # isolate its nested dictionaries (the subcategory)
-#     inner_children = outer_children.get('children')
-
-#     # return each category + sub-category pair
-#     for child in inner_children :
-#         sub_category = child.get('permalink')
-#         new_category = pd.DataFrame(data = {'category' : [category[0]], 'sub_category' : [sub_category]})
-#         categories = categories.append(new_category, ignore_index = True)
